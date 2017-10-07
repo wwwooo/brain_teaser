1f7bd252d5203d18c8926d9378c804de5b8864c7
wwwooo <malinovsvska@gmail.com>
2017-10-07 19:46:21 +0300

    update js

--- js/main.js
+++ js/main.js
@@ -7,22 +7,29 @@ $(function () {
         }
     };
 
-    $('#game').click(function(event) {
+    $('#brain_teaser').click(function(event) {
         var $target = $(event.target);
+        if ($target[0].classList.contains('brain_teaser__arrow')) {
+            var n_cell = $target.attr('cell');
 
-        if ($target[0].tagName == 'TH') {
-            var num = $target.prevAll().length;
-
-            if (num == 0) {
-                $target.siblings().each(function() {
+            if (n_cell) {
+                $(this).find('[cell=' + n_cell + ']:not(.brain_teaser__arrow)').each(function() {
                     reverse_cons_and_pros($(this));
                 });
             } else {
-                $target.parent().siblings().each(function() {
-                    var $el = $(this).children().eq(num);
-                    reverse_cons_and_pros($el);
-                })
+                $target.parent().find('[cell]').each(function() {
+                    reverse_cons_and_pros($(this));
+                });
             }
         }
     })
+
+    //fill table
+    $('[value="-"]').each(function() {
+        $(this).text('-');
+    });
+
+    $('[value="+"]').each(function() {
+        $(this).text('+');
+    });
 });
