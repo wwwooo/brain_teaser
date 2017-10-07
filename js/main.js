b67841a05e6ff31fbaf226b9e66f91802c93fddf
wwwooo <malinovsvska@gmail.com>
2017-10-07 21:13:46 +0300

    upgrate js

--- js/main.js
+++ js/main.js
@@ -9,27 +9,18 @@ $(function () {
 
     $('#brain_teaser').click(function(event) {
         var $target = $(event.target);
-        if ($target[0].classList.contains('brain_teaser__arrow')) {
-            var n_cell = $target.attr('cell');
+        if ($target.is('[arrow]')) {
+            var n_cell = $target.attr('arrow');
 
             if (n_cell) {
-                $(this).find('[cell=' + n_cell + ']:not(.brain_teaser__arrow)').each(function() {
+                $(this).find('[cell=' + n_cell + ']:not([arrow])').each(function() {
                     reverse_cons_and_pros($(this));
                 });
             } else {
-                $target.parent().find('[cell]').each(function() {
+                $target.closest('tr').find('[cell]').each(function() {
                     reverse_cons_and_pros($(this));
                 });
             }
         }
-    })
-
-    //fill table
-    $('[value="-"]').each(function() {
-        $(this).text('-');
-    });
-
-    $('[value="+"]').each(function() {
-        $(this).text('+');
     });
 });
